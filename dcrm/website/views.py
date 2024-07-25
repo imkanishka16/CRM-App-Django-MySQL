c59798c70866b18ed40c3249ec234af7adeabaa0
imkanishka16 <kanishkachathu16805@gmail.com>
2024-07-25 14:29:10 +0530

    logout

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,15 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    # check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
+        #Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in!")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error logging in, Please try again....")
+            return redirect('home')
 
-def login_user(request):
-    pass
+    else:
+        return render(request, 'home.html', {})
 
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, "You have been logged out!")
+    return redirect('home')
