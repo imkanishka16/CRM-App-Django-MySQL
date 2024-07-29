d30cf7bec718d40aec624b57bab88d8dde56aa93
imkanishka16 <kanishkachathu16805@gmail.com>
2024-07-29 17:46:21 +0530

    view user record delete user

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Records
 
 
@@ -50,3 +50,49 @@ def register_user(request):
         form = SignUpForm()
         return render(request, 'register.html', {'form':form})
     return render(request, 'register.html', {'form': form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        #Look up Records
+        customer_record = Records.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(request, "You must be logged in to view that page..")
+        return redirect('home')
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_id = Records.objects.get(id=pk)
+        delete_id.delete()
+        messages.success(request, "Record delete successfully!..")
+        return redirect('home')
+
+    else:
+        messages.success(request, "You must be logged in to do that..")
+        return redirect('home')
+
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record Added")
+                return redirect('home')
+
+        return render(request, 'add_record.html', {'form': form})
+
+    else:
+        messages.success(request, "You must be logged in..")
+        return redirect('home')
+
+
+
+
+
+
+
+
